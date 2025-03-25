30f17ba46c4344d55f01b896c3ebac9266b721fa
Dannygt1997 <daniel.munoz@telusinternational.com>
2025-03-25 19:54:21 +0000

    Tue, Mar 25, 2025, 1:54 PM -06:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -8,26 +8,27 @@ int main(void) // Main function, it asks you for the amout of levels you want
     int height;
     do
     {
-        get_int("What height do you want ");
-    }
-    
-    while (height < 1 && height > 100);
-        for (int i = 1; i <= height; i++)
-        {
-            print_pyramid_row(i, height);
-            printf("\n");
-        }
-        return 0;
+        height = get_int("What height do you want ");
     }
 
-void print_pyramid_row (int amount, int height) // This prints how many rows and spaces we need for the pyramid
+    while (height < 1 || height > 100);
+    for (int i = 1; i <= height; i++)
+    {
+        print_pyramid_row(i, height);
+        printf("\n");
+    }
+    return 0;
+}
+
+void print_pyramid_row(int amount,
+                       int height) // This prints how many rows and spaces we need for the pyramid
 {
-    for (int i = 0; i < height - amount ; i++) // Prints empty Spaces
+    for (int i = 0; i < height - amount; i++) // Prints empty Spaces
     {
         printf(" ");
     }
     for (int i = 0; i < amount; i++)
     {
-        printf("#"); //Prints #
+        printf("#"); // Prints #
     }
 }
