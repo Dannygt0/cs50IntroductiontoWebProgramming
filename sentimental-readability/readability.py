59414fcaf15f40096a7c88f0c18768a9bbd7ebc3
Dannygt1997 <daniel.munoz@telusinternational.com>
2026-01-12 00:31:43 +0000

    Sun, Jan 11, 2026, 6:31 PM -06:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -2,6 +2,7 @@ text = input("Enter the text you would like to examine: ")
 letters = 0
 words = 1
 sentences = 0
+
 for char in text:
     if char .isalpha():
      letters += 1
@@ -9,7 +10,8 @@ for char in text:
         words +=1
     elif char == "." or char == "!" or char == "?":
         sentences +=1
-
+index = 0.0588 *(letters / 100) - 0.296 * (sentences / 100) - 15.8
 print(letters)
 print(words)
 print(sentences)
+print(index)
