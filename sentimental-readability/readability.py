9c8561784e65d257fe2950775ad47a74c74d3c3e
Dannygt1997 <daniel.munoz@telusinternational.com>
2026-01-12 00:59:18 +0000

    Sun, Jan 11, 2026, 6:59 PM -06:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -7,18 +7,18 @@ for char in text:
     if char .isalpha():
      letters += 1
     elif char == " ":
-        words +=1
+        words += 1
     elif char == "." or char == "!" or char == "?":
-        sentences +=1
+        sentences += 1
 
 l = 100 * (letters / words)
 s = 100 * (sentences / words)
 index = 0.0588 * l - 0.296 * s - 15.8
 grade = round(index)
 
-if grade => 16:
+if grade >= 16:
     print("Grade 16+")
 elif grade < 1:
-   print("Before Grade 1")
-else :
+    print("Before Grade 1")
+else:
     print(f"Grade {grade}")
