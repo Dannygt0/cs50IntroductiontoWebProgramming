59f0618068e95679862d249221fb5942811ef04e
Dannygt1997 <daniel.munoz@telusinternational.com>
2026-01-15 19:08:02 +0000

    Thu, Jan 15, 2026, 1:08 PM -06:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,3 +1,5 @@
 SELECT description FROM crime_scene_reports WHERE month = 7 AND day = 28 AND year = 2025 AND street = 'Humphrey Street';
 
 SELECT name, transcript FROM interviews WHERE month = 7 AND day = 28 AND year = 2025 AND transcript LIKE '%bakery%';
+
+SELECT license_plate FROM bakery_security_logs WHERE hour = 10 AND minute >= 15 AND minute <= 25 AND activity = 'exit';
