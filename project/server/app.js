1300db24b6c76edc1b00e869bff5f09eed9a5965
Dannygt1997 <daniel.munoz@telusinternational.com>
2026-01-31 07:37:27 +0000

    Sat, Jan 31, 2026, 1:37 AM -06:00

--- project/server/app.js
+++ project/server/app.js
@@ -8,6 +8,11 @@ app.get('/', (req, res) => {
     res.json({ message: "Hello from the server!"});
 });
 
+app.get('/city/:name', (req, res) => {
+    const cityName = req.params.name;
+    res.json({ message: `You are looking for info about ${cityName}` });
+});
+
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
