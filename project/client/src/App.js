ed0669c473384698169c7942ec5eb58b4f94c8e8
Dannygt1997 <daniel.munoz@telusinternational.com>
2026-02-01 00:43:16 +0000

    Sat, Jan 31, 2026, 6:43 PM -06:00

--- project/client/src/App.js
+++ project/client/src/App.js
@@ -3,17 +3,18 @@ import axios from 'axios';
 
 function App() {
   const [city, setCity] = useState('');
+  const [photos, setPhotos] = useState([]);
 
   const searchCity = async () => await axios.get(`https://reimagined-journey-pj9gpv5q69wrh767x-3000.app.github.dev/photos?query=${city}`){
     const response = setPhotos(response.data)
   }
-  const [photos, setPhotos] = useState([]);
+
 
   return (
     < div >
       <input value={city} onChange={(e) => setCity(e.target.value)}></input>
       <button onClick={searchCity}>Submit</button>
-      {photos.map(photo => (<img src= {photo.urls.small}></img>))}
+      {photos.map(photo => (<img src={photo.urls.small}></img>))}
     </div >
   )
 
