feb0f882c36686dea0bd452b66671a31b56deaed
Dannygt1997 <daniel.munoz@telusinternational.com>
2026-02-01 00:48:29 +0000

    Sat, Jan 31, 2026, 6:48 PM -06:00

--- project/client/src/App.js
+++ project/client/src/App.js
@@ -5,18 +5,19 @@ function App() {
   const [city, setCity] = useState('');
   const [photos, setPhotos] = useState([]);
 
-  const searchCity = async () => await axios.get(`https://reimagined-journey-pj9gpv5q69wrh767x-3000.app.github.dev/photos?query=${city}`);
-  {
-    setPhotos(response.data);
-  } catch (error){
-    console.error('Error fetching photos: error);
+  const searchCity = async () => {
+    try {
+      const response = await axios.get(`https://reimagined-journey-pj9gpv5q69wrh767x-3000.app.github.dev/photos?query=${city}`);
+      setPhotos(response.data);
+    } catch (error) {
+      console.error('Error fetching photos: error');
   }
-}
+  };
 
 
   return (
     < div >
-      <input value={city} onChange={(e) => setCity(e.target.value)}></input>
+      <input value={city} onChange={(e) => setCity(e.target.value)}/>
       <button onClick={searchCity}>Submit</button>
       {photos.map(photo => (<img src={photo.urls.small}></img>))}
     </div >
